413dfb0764263d4ab0460a574e2d4a5bd68d8917
Ryan Adam <ryancadam88@gmail.com>
2020-09-14 14:40:07 -0500

    Updates

---
+++ precache-manifest.3fe3ab7fbecd4714e9ed0b91907d0a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a27bf792b1fb30cb74d99254a2af354",
+    "url": "/react-text-collage.io/index.html"
+  },
+  {
+    "revision": "4d6cc509b674f00057d4",
+    "url": "/react-text-collage.io/static/css/main.2b6e4027.chunk.css"
+  },
+  {
+    "revision": "b3bf6bc3f8557e57270b",
+    "url": "/react-text-collage.io/static/js/2.1356f18c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-text-collage.io/static/js/2.1356f18c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d6cc509b674f00057d4",
+    "url": "/react-text-collage.io/static/js/main.6f901ed6.chunk.js"
+  },
+  {
+    "revision": "63b8d0357b8cb67bc361",
+    "url": "/react-text-collage.io/static/js/runtime-main.cded5566.js"
+  }
+]);
